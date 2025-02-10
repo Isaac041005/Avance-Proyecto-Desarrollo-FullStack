63b4f743b8e9617d7ad314affd242f60fca1260c
Isaac041005 <al07003035@tecmilenio.mx>
2025-02-10 14:47:44 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  username: { type: String, required: [true, "El nombre de usuario es obligatorio"], unique: true },
+  password: { type: String, required: [true, "La contraseña es obligatoria"] }
 });
 
-// Encriptar la contraseña antes de guardarla
+// 📌 Encriptar la contraseña antes de guardar
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
@@ -14,4 +14,3 @@ UserSchema.pre("save", async function (next) {
 });
 
 module.exports = mongoose.model("User", UserSchema);
-
